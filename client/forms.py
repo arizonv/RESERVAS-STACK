1dad3b59b02c3893c9871deb07858ba76949309d
arizonv <a.vasgarridoe@gmail.com>
2023-07-10 22:50:01 -0400

    css HOME

--- client/forms.py
+++ client/forms.py
@@ -4,12 +4,47 @@ from django.utils import timezone
 import datetime
 from .models import Cliente
 import calendar
+from django.core.exceptions import ValidationError
 
 
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ['rut', 'sexo']
+
+    def clean_rut(self):
+        rut = self.cleaned_data['rut']
+        rut = rut.upper()  # Convertir el Rut a mayúsculas
+        rut = rut.replace(".", "")  # Eliminar los puntos del Rut
+        rut = rut.replace("-", "")  # Eliminar el guión del Rut
+
+        # Extraer el dígito verificador y el número base del Rut
+        dv = rut[-1]
+        rut_base = rut[:-1]
+
+        # Validar que el número base del Rut sea un entero
+        try:
+            int(rut_base)
+        except ValueError:
+            raise forms.ValidationError('El Rut ingresado no es válido.')
+
+        # Calcular el dígito verificador esperado
+        suma = 0
+        multiplicador = 2
+        for digito in reversed(rut_base):
+            suma += int(digito) * multiplicador
+            multiplicador += 1
+            if multiplicador == 8:
+                multiplicador = 2
+
+        resto = suma % 11
+        dv_esperado = str(11 - resto) if resto > 1 else '0'
+
+        # Comparar el dígito verificador calculado con el ingresado
+        if dv != dv_esperado:
+            raise forms.ValidationError('El Rut ingresado no es válido.')
+
+        return rut
 
 
 class ReservaForm(forms.ModelForm):
@@ -38,4 +73,7 @@ class ReservaForm(forms.ModelForm):
             raise forms.ValidationError("La fecha ya está reservada.")
 
         return dia
+
+
+
 
