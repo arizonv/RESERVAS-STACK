6a082edf27052030b347fc721e92b58fd7b2d66e
arizonv <a.vasgarridoe@gmail.com>
2023-07-16 18:02:03 -0400

    en revision

--- servicio/apps.py
+++ servicio/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 import threading
 
-class ServicesConfig(AppConfig):
+class ServiciosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'servicio'
